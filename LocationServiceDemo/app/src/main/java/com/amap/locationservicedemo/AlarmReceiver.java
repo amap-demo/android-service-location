2d3ec733149867e6fb1277a3ad62b3f7a3c6fdf5
li.gen <li.gen@alibaba-inc.com>
2016-12-15 20:46:08 +0800

    增加自动点亮屏幕代码，以解决cpu休眠导致的网络无法连接问题

--- LocationServiceDemo/app/src/main/java/com/amap/locationservicedemo/AlarmReceiver.java
+++ LocationServiceDemo/app/src/main/java/com/amap/locationservicedemo/AlarmReceiver.java
@@ -3,6 +3,7 @@ package com.amap.locationservicedemo;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.PowerManager;
 import android.util.Log;
 
 /**
@@ -17,14 +18,23 @@ import android.util.Log;
  * 类说明：
  */
 public class AlarmReceiver extends BroadcastReceiver {
-
+    private String TAG = "AlarmReceiver";
+    private PowerManager powerManager;
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals("LOCATION_CLOCK")) {
-            Log.e("ggb", "--->>>   onReceive  LOCATION_CLOCK");
+            Log.e(TAG, "--->>>   onReceive  LOCATION_CLOCK");
             Intent locationIntent = new Intent(context, LocationService.class);
             context.startService(locationIntent);
+            if (powerManager == null) {
+                //针对熄屏后cpu休眠导致的无法联网、定位失败问题,通过定期点亮屏幕实现联网,本操作会导致cpu无法休眠耗电量增加,谨慎使用
+                powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+                PowerManager.WakeLock wl = powerManager.newWakeLock(PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.SCREEN_DIM_WAKE_LOCK, "bright");
+                wl.acquire();
+                //点亮屏幕
+                wl.release();
+                //释放
+            }
         }
-
     }
 }
